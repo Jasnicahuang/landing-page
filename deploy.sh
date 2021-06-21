59cd33533eddd667598ee1203456a7cb22e523f1
Jasnicahuang <jasnica@alumni.pcr.ac.id>
2021-06-21 22:49:21 +0700

    setup web

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /var/www/html/
+sudo chown -R www-data:www-data /var/www/
+sudo chmod -R 755 /var/www/
+sudo -- sh -c "echo $(hostname -I | awk '{print $2}') landingpage.com >> /etc/hosts"
