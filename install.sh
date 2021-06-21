7660a53a16adf3fd4600db4746e14e0edca94c0d
Jasnicahuang <jasnica@alumni.pcr.ac.id>
2021-06-21 22:33:53 +0700

    Add remove index.html

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo chown -R www-data:www-data /var/www
+sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 755 /var/www
+sudo rm -rf /var/www/html/index.html
