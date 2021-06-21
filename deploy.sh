05278e98dff05d9fcec3c409d283b96e5a7b1b61
Jasnicahuang <jasnica@alumni.pcr.ac.id>
2021-06-21 22:53:58 +0700

    setup web

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ rm -rf /var/www/html/
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R 755 /var/www/
 sudo -- sh -c "echo $(hostname -I | awk '{print $2}') landingpage.com >> /etc/hosts"
+sudo service nginx restart
